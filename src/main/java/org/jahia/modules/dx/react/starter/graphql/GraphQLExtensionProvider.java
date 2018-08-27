d20ba19e4856607e75818fd0d1e9747c67023907
stefan <ssavu@jahia.com>
2018-08-27 11:00:49 -0400

    Add GraphQLProvider implementation to module.

---
+++ src/main/java/org/jahia/modules/dx/react/starter/graphql/GraphQLExtensionProvider.java
@@ -0,0 +1,21 @@
+package org.jahia.modules.dx.react.starter.graphql;
+
+import org.jahia.modules.graphql.provider.dxm.DXGraphQLExtensionsProvider;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+@Component(service = DXGraphQLExtensionsProvider.class, immediate = true)
+public class GraphQLExtensionProvider implements DXGraphQLExtensionsProvider {
+
+    @Override
+    public Collection<Class<?>> getExtensions() {
+        List<Class<?>> extensions = new ArrayList<>();
+        //Add extensions as follows:
+        //extensions.add(GraphQLExtension.class);
+        return extensions;
+    }
+}
